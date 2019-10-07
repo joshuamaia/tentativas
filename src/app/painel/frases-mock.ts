06a553a239449af5471fbd27830abcccbef9141e
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-10-07 08:22:06 -0300

    Jogo das tentativas para acertar as cores

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+export const FRASES: Frase[] = [
+  { fraseEng: 'blue', frasePtBr: 'azul' },
+  { fraseEng: 'red', frasePtBr: 'vermelho' },
+  { fraseEng: 'black', frasePtBr: 'preto' },
+  { fraseEng: 'white', frasePtBr: 'branco' }
+];
